b9b7c0d570510d9fc80c81abfce62a634743d11a
fellow_engineer <71657493+fellowEngineer@users.noreply.github.com>
2022-09-07 18:22:27 +0530

    these are come from array ADT tuitorials (#13)

---
+++ DSA/array/mergeTwoSortedArray.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int* mergeTwoSortedArray (int a[], int b[],int al,int bl)
+{
+	int t = al+bl;
+	int *p = new int [t];
+	int i=0,j=0,k=0;
+	
+	for(; (i<al and j<bl);)
+	{
+		if(a[i] < b[j])
+		{
+			p[k] = a[i];
+			i++;
+			k++;
+		}
+		else if(b[j] < a[i])
+		{
+			p[k] = b[j];
+			j++;
+			k++;
+		}
+	}
+	for(;i<al and k<t;i++,k++)
+		p[k] = a[i];
+	
+	for(;j<bl and k<t;j++,k++)
+		p[k] = b[j];
+	
+	
+	return p;
+}
+
+
+int main ()
+{
+	int x[7] = {1,3,5,7,9,11,13};
+	int y[6] = {2,4,6,8,10,12};
+	int n=6,m=7;
+	
+	int* p = mergeTwoSortedArray(x,y,m,n);
+	
+	for(int i=0; i< n+m;i++)
+		cout << p[i] << " ";
+	
+	return 0;	
+}
---
+++ DSA/array/search.cpp
@@ -0,0 +1,11 @@
+﻿bool search (int* p, int l, int key)
+{
+	for(int i=0;i<l;i++)
+	{
+		if(p[i] == key)
+		{
+			return true;
+		}
+	}
+	return false;
+}
