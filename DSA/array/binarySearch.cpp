4419f5e2edbaee70196fa15e3b5b9dd92871f131
fellowEngineer <bsuman1921@gmail.com>
2022-09-07 18:18:22 +0530

    these are come from array ADT tuitorials

---
+++ DSA/array/binarySearch.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+int n = 10;
+
+int bS (int key)
+	{
+		int low=0, high = n-1, mid;
+		
+		while (low <= high)
+		{
+			mid = (low+high)/2;
+			
+			if(key < arr[mid])
+				high = mid-1;
+			if(key > arr[mid])
+				low = mid+1;
+			if (key == arr[mid])
+				return mid;
+		}
+		return -1;
+	}
+int r_bS (int l,int h,int arr[],int key)
+	{
+		int mid = (l+h)/2;
+		if(l>h)
+			return -1;
+		
+		if(key == arr[mid])
+			return mid;
+		if(key < arr[mid])
+			return r_bS(l,mid-1,arr,key);
+		if(key > arr[mid])
+			return r_bS(mid+1,h,arr,key);
+	}
+
+int main ()
+{
+	cout << "bS " << bS(7) << "\n";
+	cout << "r_bS " << r_bS(0,n-1,arr,7) << "\n";
+
+	return 0;	
+}
---
+++ DSA/array/negetive_positive_replacing.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+void swap_ (int& a, int& b)
+{
+	int tem = a;
+	a = b;
+	b = tem;
+}
+
+void neg_pos_replace (int a[], int n)
+{
+	int i=0, j=n-1;
+	
+	while (i < j)
+	{
+		while (a[i] < 0) i++;
+		while (a[j] >= 0) j--;
+		
+		if(i < j)
+			swap_ (a[i],a[j]);
+	}
+	
+}
+// the output not sorted , -ve are in head of the
+// list and +ve are on tail of the list....
+
+
+int main ()
+{
+	int n = 8;
+	int a[8] = {1,3,-6,-8,10,-13,23,-9};
+	neg_pos_replace(a,n);
+
+	for(int x : a)
+		cout << x << " ";
+	
+	return 0;	
+}
